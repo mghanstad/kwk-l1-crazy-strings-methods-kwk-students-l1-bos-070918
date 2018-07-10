099b47b39b173600d61e6154171e42a841ccaab8
mghanstad <mghanstad@gmail.com>
2018-07-10 19:16:47 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,5 @@
-# write your method here
-
-def crazy_strings(a,b)
-
+def crazy_strings("Hello" , "Friends")
+Hello = "Hello".reverse.upcase
+Friends = "Friends".swapcase.gsub("s","z")
 end
+puts crazy_strings
